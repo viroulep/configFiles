2e95c065d9a8382905a72898f3514ce8e3c0c626
Philippe Virouleau <philippe.virouleau@imag.fr>
2013-04-09 18:05:07 +0200

    ajout cmus

--- .i3/now_playing.sh
+++ .i3/now_playing.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-DAP=`cat $HOME/dap-now-playing`
-if [ "$DAP" == "" ]; then
-    DAP_ARTIST=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
-    DAP_TITLE=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+NP=`cat $HOME/now-playing`
+if [ "$NP" == "" ]; then
+    NP_ARTIST=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
+    NP_TITLE=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
     if [ $? -eq 0 ]; then
-        DAP="$DAP_ARTIST - $DAP_TITLE"
+        NP="$NP_ARTIST - $NP_TITLE"
     fi
 fi
-echo $DAP
+echo $NP
