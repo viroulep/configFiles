689c68ec135b44d68ab3c57e74be51d974c33a88
Philippe Virouleau <philippe.virouleau@imag.fr>
2013-04-09 14:28:00 +0200

    ajout support 'now_playing'

---
+++ .i3/now_playing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+DAP=`cat $HOME/dap-now-playing`
+if [ "$DAP" == "" ]; then
+    DAP_ARTIST=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
+    DAP_TITLE=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+    if [ $? -eq 0 ]; then
+        DAP="$DAP_ARTIST - $DAP_TITLE"
+    fi
+fi
+echo $DAP
---
+++ .i3/run_i3status.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Public Domain
+# (someone claimed the next lines would be useful for…
+#  people. So here goes: © 2012 Stefan Breunig
+#  stefan+measure-net-speed@mathphys.fsk.uni-heidelberg.de)
+
+# append i3status output to the measure-net-speed’s one.
+# the quote and escape magic is required to get valid
+# JSON output, which is expected by i3bar (if you want
+# colors, that is. Otherwise plain text would be fine).
+# For colors, your i3status.conf should contain:
+# general {
+#   output_format = i3bar
+# }
+
+# i3 config looks like this:
+# bar {
+#   status_command measure-net-speed-i3status.bash
+# }
+
+i3status | (read line && echo $line && read line && echo $line && while :
+do
+  read line
+  dat=$(now_playing.sh 2>/dev/null)
+  dat="[{ \"full_text\": \"${dat}\", \"color\":\"#5FAFFF\" },"
+  echo "${line/[/$dat}" || exit 1
+done)
+
+
+
+
