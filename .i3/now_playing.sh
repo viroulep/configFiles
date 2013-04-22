c73c24120bef80daf4d4d163aeaa9525464fe1be
Philippe Virouleau <philippe.virouleau@imag.fr>
2013-04-22 19:29:41 +0200

    urxvtd + vimpc status

--- .i3/now_playing.sh
+++ .i3/now_playing.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 NP=`cat $HOME/now-playing`
 if [ "$NP" == "" ]; then
-    NP_ARTIST=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
-    NP_TITLE=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
+    NP_ARTIST=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$`
+    NP_TITLE=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' 2>/dev/null|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$`
     if [ $? -eq 0 ]; then
         NP="$NP_ARTIST - $NP_TITLE"
     fi
 fi
 if [ "$NP" == "" ]; then
-    NP=`mpc status | grep -v -e "volume" -e "\[playing\]"`
+    NP=`mpc status 2>/dev/null | grep -v -e "ERROR" -e "volume" -e "\[playing\]"`
+    if [ $? -ne 0 ]; then
+        NP="Not Playing"
+    fi
 fi
 echo $NP
