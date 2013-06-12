46bc45920f08ad34a1c01eeb76db595dc64b2f13
Philippe Virouleau <philippe.virouleau@imag.fr>
2013-06-12 15:59:48 +0200

    ajout mutt

--- install_home.sh
+++ install_home.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-FILES=".vimperatorrc .vimpcrc .inputrc .zshrc .zsh_aliases .zsh_zle .zsh_helpers .zsh_global .gitconfig .Xdefaults .xsession .i3 .i3status.conf .vim .vimrc .config/dwb .config/dunst"
+FILES=".mutt .muttrc .abook .fetchmailrc .mailcap .vimperatorrc .vimpcrc .inputrc .zshrc .zsh_aliases .zsh_zle .zsh_helpers .zsh_global .gitconfig .Xdefaults .xsession .i3 .i3status.conf .vim .vimrc .config/dwb .config/dunst"
 DIR=`pwd`
 for f in $FILES;
 do
