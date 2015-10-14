60010b1bf1143f07cf4764f1b5ae0ca222efc8de
Philippe Virouleau <philippe.virouleau@imag.fr>
2015-10-14 16:36:21 +0200

    Added new setup
    Changed output names/sound output
    Added ncmpcpp stuff

--- .i3/scripts/dmenu_power.py
+++ .i3/scripts/dmenu_power.py
@@ -8,7 +8,7 @@ termprefix='urxvt -e '
 
 choices ={
         'lp(lock input)\n':'xtrlock',
-        'suspend\n':'setupscreens LVDS1;i3-msg workspace 10;i3lock -c 111111;sudo pm-suspend;',
+        'suspend\n':'setupscreens LVDS-0;i3-msg workspace 10;i3lock -c 111111;sudo pm-suspend;',
         'shutdown\n':'gksudo "shutdown -h now"',
         'lock(lock X)\n':'i3-msg workspace 10; i3lock -d -c 111111',
         'reboot\n':'gksudo "shutdown -r now"'
