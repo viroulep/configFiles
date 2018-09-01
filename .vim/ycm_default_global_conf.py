eb30907c9a2f9c2cd32c391698a5c5394a7b063f
Philippe Virouleau <philippe.44@gmail.com>
2018-09-01 16:09:59 +0200

    update ycmd default conf

--- .vim/ycm_default_global_conf.py
+++ .vim/ycm_default_global_conf.py
@@ -1,19 +1,35 @@
 import sys
-import ycm_sys_conf as conf
+import os
 from os.path import expanduser
 
 homeDir = expanduser("~")
+default_cfamily_flags = ['-Wall', '-Wextra', '-fexceptions', '-I', homeDir + "/local/include", '-I', '.']
+all_flags = {
+  "c": default_cfamily_flags + ['-std=c99', '-x', 'c'],
+  "c++": default_cfamily_flags + ['-std=c++11', '-x', 'c++'],
+  "": [],
+}
 
-#Default for project is C files
-conf.flags += ['-std=c99', '-x', 'c']
-conf.flags += [
-'-Wall',
-'-Wextra',
-'-fexceptions',
-'-I',
-homeDir + '/local/include',
-'-I',
-'.',
-]
+def GetLanguage(filename):
+  extension = os.path.splitext( filename )[ 1 ]
+  if extension in [ '.h', '.hh', '.c', '.cc' ]:
+    return "c"
+  elif extension in [ '.hxx', '.hpp', '.cpp', '.cxx' ]:
+    return "c++"
+  else:
+    return ""
 
-FlagsForFile = conf.FlagsForFile
+def Settings(**kwargs):
+  language = kwargs['language']
+  filename = kwargs['filename']
+  with open("/tmp/titi.txt", "w") as f:
+    titi = "{}:{}:{}".format(*kwargs)
+    f.write(titi)
+    f.write(language)
+    f.write(filename)
+
+  if language == "cfamily":
+    return {
+      'flags': all_flags[GetLanguage(filename)],
+    }
+  return {}
